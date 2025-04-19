ef4fdd3d44018f6b289773811bf01ccd816b942f
Houjie80 <86527356+Houjie80@users.noreply.github.com>
2025-04-20 03:48:24 +0700

    Update clk-pll.c

--- drivers/clk/meson/clk-pll.c
+++ drivers/clk/meson/clk-pll.c
@@ -590,7 +590,8 @@ const struct clk_ops meson_secure_clk_pll_ops = {
 	.enable		= meson_secure_clk_pll_enable,
 	.disable	= meson_secure_clk_pll_disable
 };
-EXPORT_SYMBOL_GPL(meson_secure_clk_pll_ops);
+EXPORT_SYMBOL_NS_GPL(meson_clk_pll_ro_ops, CLK_MESON);
+
 MODULE_DESCRIPTION("Amlogic PLL driver");
 MODULE_AUTHOR("Carlo Caione <carlo@endlessm.com>");
 MODULE_AUTHOR("Jerome Brunet <jbrunet@baylibre.com>");
